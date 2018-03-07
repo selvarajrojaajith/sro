ba4ebfe540d1524702948f8d58a2cb57d103bc1d
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-07 16:01:54 +0530

    char.c

---
+++ char.c
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int alphabets,i;
+printf("\nEnter any string:");
+gets(string);
+{
+if(string[i]>='a' && string[i]<='A';
+{
+alphabets++;
+}
+if(string[i]>='0' && string[i]='9';
+}
+{
+i++;
+}
+printf("\n Enter the special character in a given string");
+printf("\n special character =[%d]\n,others);
+return 0
+}
